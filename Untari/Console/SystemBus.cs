98e6acd33cfbd3372cead0e43cbbba040146c129
Mensch, Adam <adam.mensch@dematic.com>
2017-07-17 15:16:22 -0500

    Added RAM device

--- Untari/Console/SystemBus.cs
+++ Untari/Console/SystemBus.cs
@@ -8,5 +8,40 @@ namespace Untari.Console
 {
     public class SystemBus
     {
+        List<IBusDevice> devices;
+
+        public SystemBus()
+        {
+            devices = new List<IBusDevice>();
+        }
+
+        public void AddBusDevice(IBusDevice device)
+        {
+            devices.Add(device);
+        }
+
+        public byte GetByte(int address)
+        {
+            foreach(IBusDevice device in devices)
+            {
+                if(device.IsChipSelected(address))
+                {
+                    return GetByte(address);
+                }
+            }
+            throw new InvalidOperationException("No device for address " + address.ToString("X4"));
+        }
+
+        public void WriteByte(int address, byte data)
+        {
+            foreach(IBusDevice device in devices)
+            {
+                if(device.IsChipSelected(address))
+                {
+                    device.WriteByte(address, data);
+                    break;
+                }
+            }
+        }
     }
 }
